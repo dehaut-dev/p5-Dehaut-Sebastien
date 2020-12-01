9b0914ce0bc7074b6a17e2418442b1b18d74e818
karkass13127 <dehaut.jean.seb@gmail.com>
2020-12-01 18:49:46 +0100

    creation de l'api
    mise en place de l'api via fetch et de la structure de la page principal

---
+++ index.js
@@ -0,0 +1,19 @@
+const api = "http://localhost:3000/api/teddies";
+
+fetch(api)
+    .then(response => response.json())
+    .then(teddies => {
+        const container = document.getElementById("products");
+        for (const teddy of teddies) {
+
+            container.innerHTML += `
+        <div class="col-md-5 mr-auto text-center mt-5 justify-content-between border">
+            <h1>${teddy.name}</h1>
+            <img class="col-md-auto center-block" src="${teddy.imageUrl}"></img>
+            <div>${teddy.price} €</div>
+            <a type="button" class="btn btn-danger my-2" href="produits.html?${teddy._id}">information</a>
+        </div>`
+
+        }
+        console.log(teddies);
+    })
